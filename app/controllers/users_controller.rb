f73f69154ccf7102def43c636164f178d9e027d9
SugiKen <ynmk1995@gmail.com>
2018-04-16 19:08:15 +0900

    validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < SessionsController
 
     # みんな0の'tenki'に
     @user.function_type = 0
+
+    if user_params[:prefecture_id].blank?
+      redirect_to settings_path, notice: '都道府県を設定してください。'
+      return false
+    end
+
+    if user_params[:modify_name].blank?
+      redirect_to settings_path, notice: '変更後の名前を設定してください。'
+      return false
+    end
+
     if @user.update(user_params)
       redirect_to settings_path, notice: '設定を更新しました。'
     else
