524fe335682f9accf5c7b1c5a059df9b478966eb
SugiKen <ynmk1995@gmail.com>
2018-04-16 19:18:06 +0900

    夜は晴れの場合だけstarに

--- app/models/tenki.rb
+++ app/models/tenki.rb
@@ -19,35 +19,40 @@ class Tenki
     @result['weather'][0]['icon']
   end
 
+  def weather_id
+    @result['weather'][0]['id']
+  end
+
   def emoji
     moji = ''
-    if icon[2] == 'n' #夜の場合
-      moji = "\u{1F31F}"
-    else
-      case icon
-      when '01d' #晴れ
-        moji = "\u{2600}"
-      when '02d' #晴れ+曇り
-        moji = "\u{26C5}"
-      when '03d' #曇り
-        moji = "\u{2601}"
-      when '04d' #曇り
-        moji = "\u{2601}"
-      when '09d' #小雨
-        moji = "\u{1F302}"
-      when '10d' #雨
-        moji = "\u{2614}"
-      when '11d' #雷
-        moji = "\u{26A1}"
-      when '13d' #雪
-        moji = "\u{2744}"
-      when '50d' #霧
-        moji = "\u{1F301}"
+    case icon[0..1]
+    when '01' #晴れClear
+      if icon[2] == 'n'
+        moji = "\u{1F31F}"
       else
         moji = "\u{2600}"
       end
+    when '02' #晴れ+曇りClouds
+      moji = "\u{26C5}"
+    when '03' #曇りClouds
+      moji = "\u{2601}"
+    when '04' #曇りClouds
+      moji = "\u{2601}"
+    when '09' #小雨Drizzle/Rain
+      moji = "\u{1F302}"
+    when '10' #雨Rain
+      moji = "\u{2614}"
+    when '11' #雷Thunderstorm
+      moji = "\u{26A1}"
+    when '13' #雪Snow/freezing rain
+      moji = "\u{2744}"
+    when '50' #霧/Atmosphere
+      moji = "\u{1F301}"
+    else
+      moji = "\u{2600}"
     end
 
     moji
   end
+
 end
