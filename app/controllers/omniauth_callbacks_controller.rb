51c7aff8b5452ed325f6b6a618f2700f15a60dc8
SugiKen <ynmk1995@gmail.com>
2018-04-18 22:08:09 +0900

    fix 全部東京都

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,12 +6,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user.token = auth.extra.access_token.token
     @user.secret = auth.extra.access_token.secret
 
-    @user.prefecture_id = 13
     @user.save
 
     if @user.persisted?
       sign_in_and_redirect @user
     else
+      @user.prefecture_id = 13
+      @user.save
       session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
     end
